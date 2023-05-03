e9d672dbe52fe8fd5431724c505acb650b36657f
piaacv <122986202+piaacv@users.noreply.github.com>
2023-05-03 14:10:44 -0400

    subir código github

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import validator from './validator.js';
 
-console.log(validator);
+const creditCardNumber = document.getElementById("creditNumber");
+const validateButton = document.getElementById("cardButton")
+const resultvalidation = document.getElementById("resultMessage")
+
+validateButton.addEventListener("click", () => {
+  if(creditCardNumber.value === ""){
+    resultvalidation.innerHTML = "Debes ingresar el número de tu tarjeta"
+  } else{
+    const creditCardNumberArray = creditCardNumber.value.split ("");
+    const cardValidating = validator.isValid(creditCardNumberArray)
+    const numberMaskify = validator.maskify(creditCardNumberArray)
+  
+
+    if(cardValidating){
+      resultvalidation.innerHTML = "Su tarjeta n° " + numberMaskify + " es válida, puedes continuar con tu compra"
+    }else{
+      resultvalidation.innerHTML = "Su tarjeta n° " + numberMaskify + " es inválida. Intenta con otra tarjeta o cambia el método de pago"
+    }
+  }
+    
+     
+
+  creditCardNumber.value = ""
+
+}
+
+)
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,34 @@
 const validator = {
-  // ...
+  isValid : function (number){
+    const workNumber = [...number]
+    const numberReverseAndMultiplied = workNumber.reverse().map((evaluateNumber, i) => {
+      if (i % 2 !==0){
+        evaluateNumber = evaluateNumber * 2
+        if (evaluateNumber>=10){
+          return evaluateNumber - 9
+        }
+        return evaluateNumber
+      }
+      return evaluateNumber * 1
+    })
+
+    const initialValue = 0
+    const sumCardNumbers = numberReverseAndMultiplied.reduce((acum,currentValue) => {
+      return acum + currentValue
+    }, initialValue)
+
+    if(sumCardNumbers % 10 ===0){
+      return true
+    } else{
+      return false
+    }
+  },
+
+  maskify : function (numberOriginal){
+    const numberMaskify = numberOriginal.fill("x",0,-4)
+    return numberMaskify
+  }
+
 };
 
 export default validator;
